39d7aee61ad1ab8ae955213a7977451389f95293
plnvsr <pelinavsar98@gmail.com>
2023-10-11 15:33:50 +0300

    48-08

---
+++ components/ListItem.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface ListItemProps{
+    image: string, 
+    name: string, 
+    href: string
+}
+
+const ListItem: React.FC<ListItemProps> = ({
+    image, 
+    name, 
+    href
+}) => {
+    const router = useRouter();
+    return ( 
+    <div>
+        List Item!
+    </div> );
+}
+ 
+export default ListItem;
