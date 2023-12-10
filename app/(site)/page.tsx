c4b23c889c486aec6ba6295361509d4a105efc74
plnvsr <pelinavsar98@gmail.com>
2023-12-10 06:29:58 +0300

    1-46

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,9 +17,23 @@ export default function Home() {
           <h1
             className='text-white text-3xl font-semibold'>Welcome back</h1>
             <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4'></div>
-            <ListItem/>
+            <ListItem
+              image = "/images/liked.png"
+              name = "Liked Songs"
+              href ="liked" />
         </div>
       </Header>
+      <div className='mt-2 mb-7 px-6'>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-white text-2xl font-semibold'>
+            Newest songs
+          </h1>
+        </div>
+        <div>
+          List of songs
+        </div>
+
+      </div>
     </div>
   )
 }
